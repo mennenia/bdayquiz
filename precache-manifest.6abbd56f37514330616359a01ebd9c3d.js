3db2b61ea3be896e1df7f2ef29edc34c217d4e46
Maxim Cramer <maxim.cramer@autodesk.com>
2019-11-08 17:17:49 +0000

    Updates

---
+++ precache-manifest.6abbd56f37514330616359a01ebd9c3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296e918397d40b4a6ff0ed38df26e74d",
+    "url": "/bdayquiz/index.html"
+  },
+  {
+    "revision": "552eec5421d6cf16b0c1",
+    "url": "/bdayquiz/static/css/main.b5a6d08c.chunk.css"
+  },
+  {
+    "revision": "43864dde141fe84cefc5",
+    "url": "/bdayquiz/static/js/2.abbe919a.chunk.js"
+  },
+  {
+    "revision": "552eec5421d6cf16b0c1",
+    "url": "/bdayquiz/static/js/main.8212ab51.chunk.js"
+  },
+  {
+    "revision": "55ee1d608f4bb8c51e15",
+    "url": "/bdayquiz/static/js/runtime-main.443ed4be.js"
+  },
+  {
+    "revision": "b52f91a0f0d1c704f5126e265d05ed08",
+    "url": "/bdayquiz/static/media/bond.b52f91a0.jpeg"
+  },
+  {
+    "revision": "d47930a5004666a2a7f1ea11ac27404a",
+    "url": "/bdayquiz/static/media/intro.d47930a5.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bdayquiz/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "84e4dfb0c562d03003792941400e73d0",
+    "url": "/bdayquiz/static/media/worrall.84e4dfb0.jpeg"
+  }
+]);
